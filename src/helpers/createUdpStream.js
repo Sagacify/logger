fdff6f596c6394b441f15c68fce5316c8df7087c
Olivier Cuypers <oliviercuyp@gmail.com>
2019-04-01 13:01:46 +0200

    fix(createUdpStream): unref socket

--- src/helpers/createUdpStream.js
+++ src/helpers/createUdpStream.js
@@ -6,6 +6,8 @@ const createUdpStream = endpoint => {
   const splitIndex = endpoint.lastIndexOf(':');
   const port = endpoint.substring(splitIndex + 1);
   const address = endpoint.substring(0, splitIndex);
+  // Makes sure the process doesn't hang
+  socket.unref();
 
   return new Writable({
     final () { socket.close(); },
